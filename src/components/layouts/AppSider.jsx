9e984cf81dfe03c9944388112a5310aa36fb8549
Bainda <giorgibainda14@gmail.com>
2024-12-09 23:16:17 +0400

    structured layout / two cards which shows bitcoin/ethereum charts

--- src/components/layouts/AppSider.jsx
+++ src/components/layouts/AppSider.jsx
@@ -2,17 +2,11 @@ import { Layout, Card, Statistic, Typography, List, Spin } from "antd";
 import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
 import { useEffect, useState } from "react";
 import { fakeFetchCrypto, fetchAssets } from "./api";
+import { percentDifference } from "../../utils";
 
 const siderStyle = {
   padding: "1rem",
 };
-
-const data = [
-  "Racing car sprays burning fuel into crowd.",
-  "Japanese princess to wed commoner.",
-  "Australian walks 100km after outback crash",
-  "man charged over missing ",
-];
 
 export default function AppSider() {
   const [loading, setLoading] = useState(false);
@@ -24,7 +18,20 @@ export default function AppSider() {
       setLoading(true);
       const { result } = await fakeFetchCrypto();
       const assets = await fetchAssets();
-      setAssets(assets);
+      setAssets(
+        assets.map((asset) => {
+          const coin = result.find((coin) => coin.id === asset.id);
+
+          return {
+            id: asset.id,
+            grow: asset.price < coin.price,
+            growPercent: percentDifference(asset.price, coin.price),
+            totalAmount: asset.amount * coin.price,
+            totalProfit: asset.amount * coin.price - asset.amount * asset.price,
+            amount: asset.amount,
+          };
+        })
+      );
       setCrypto(result);
       setLoading(false);
     }
@@ -37,27 +44,39 @@ export default function AppSider() {
 
   return (
     <Layout.Sider width="25%" style={siderStyle}>
-      <Card style={{ marginBottom: "1rem" }}>
-        <Statistic
-          title="Active"
-          value={11.28}
-          precision={2}
-          valueStyle={{ color: "#3f8600" }}
-          prefix={<ArrowUpOutlined />}
-          suffix="%"
-        ></Statistic>
-        <List
-          size="small"
-          dataSource={data}
-          renderItem={(item) => (
-            <List.Item>
-              <Typography.Text mark>[ITEM]</Typography.Text>
-              {item}
-            </List.Item>
-          )}
-        ></List>
-      </Card>
-      <Card>
+      {assets.map((asset) => (
+        <Card key={asset.id} style={{ marginBottom: "1rem" }}>
+          <Statistic
+            title={asset.id}
+            value={asset.totalAmount}
+            precision={2}
+            valueStyle={{ color: asset.grow ? "#3f8600" : "#cf1322" }}
+            prefix={asset.grow ? <ArrowUpOutlined /> : <ArrowDownOutlined />}
+            suffix="$"
+          ></Statistic>
+          <List
+            size="small"
+            dataSource={[
+              { title: "Total Profit", value: asset.totalProfit },
+              { title: "Asset Amount", value: asset.amount, isPlain: true },
+              { title: "Difference", value: asset.growPercent },
+            ]}
+            renderItem={(item) => (
+              <List.Item>
+                <span>{item.title}</span>
+                {item.isPlain && <span>{item.value}</span>}
+                {!item.isPlain && (
+                  <Typography.Text type={asset.grow ? "success" : "danger"}>
+                    {item.value.toFixed(2)}$
+                  </Typography.Text>
+                )}
+              </List.Item>
+            )}
+          ></List>
+        </Card>
+      ))}
+
+      {/* <Card>
         <Statistic
           title="Idle"
           value={9.3}
@@ -66,7 +85,7 @@ export default function AppSider() {
           prefix={<ArrowDownOutlined />}
           suffix="%"
         ></Statistic>
-      </Card>
+      </Card> */}
     </Layout.Sider>
   );
 }
