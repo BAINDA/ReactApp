25ef729fd3338fcfffd9ec706fa2afc5446037a9
Bainda <giorgibainda14@gmail.com>
2024-12-09 21:25:15 +0400

    added components folder and layouts also organized code structure and added preloader spinner for better ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import React from "react";
+import { Layout } from "antd";
+import AppHeader from "./components/layouts/appheader";
+import AppSider from "./components/layouts/AppSider";
+import AppContent from "./components/layouts/AppContent";
+
 export default function App() {
-  return <h1>React APP</h1>;
+  return (
+    <Layout>
+      <AppHeader></AppHeader>
+      <Layout>
+        <AppSider></AppSider>
+        <AppContent></AppContent>
+      </Layout>
+    </Layout>
+  );
 }
