c7a23980800e62c5a0caa61e954d43abe7929aa9
Bainda <giorgibainda14@gmail.com>
2024-12-10 23:44:44 +0400

    added in layouts two new files for AssetsTable and PortfolioChart also added to project ChartJs Library

--- src/components/layouts/AppContent.jsx
+++ src/components/layouts/AppContent.jsx
@@ -1,5 +1,7 @@
-import { Layout } from "antd";
-
+import { Layout, Typography } from "antd";
+import { useCrypto } from "../../context/crypto-context";
+import PortfolioChart from "./PortfolioChart";
+import AssetsTable from "./AssetsTable";
 const contentStyle = {
   textAlign: "center",
   minHeight: "calc(100vh - 60px)",
@@ -9,5 +11,25 @@ const contentStyle = {
 };
 
 export default function AppContent() {
-  return <Layout.Content style={contentStyle}>Content</Layout.Content>;
+  const { assets, crypto } = useCrypto();
+
+  const cryptoPriceMap = crypto.reduce((acc, coin) => {
+    acc[coin.id] = coin.price;
+    return acc;
+  }, {});
+
+  return (
+    <Layout.Content style={contentStyle}>
+      <Typography.Title level={3} style={{ textAlign: "left", color: "#fff" }}>
+        Portofolio:{" "}
+        {assets
+          .map((asset) => asset.amount * cryptoPriceMap[asset.id])
+          .reduce((acc, val) => acc + val, 0)
+          .toFixed(2)}
+        $
+      </Typography.Title>
+      <PortfolioChart></PortfolioChart>
+      <AssetsTable></AssetsTable>
+    </Layout.Content>
+  );
 }
